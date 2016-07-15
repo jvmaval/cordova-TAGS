cc012bca556d6a5b58cd774cdcda7f798ac53c90
jvmaval <jose.v.herraez.@gmail.com>
2016-07-15 11:17:30 +0200

    asdasd

--- www/TAGS.js
+++ www/TAGS.js
@@ -1,7 +1,2 @@
-window.echo = function(natureOfTalk)
-    {
-        alert("Inside JS Interface");
-        cordova.exec(function(result){alert("Result is : "+result);},
-                     function(error){alert("Some Error happened : "+ error);},
-                     "TAGS","readTAG",[]);
-    };
+
+document.addEventListener("tagReceived",function(){alert("TAG Leido");});
