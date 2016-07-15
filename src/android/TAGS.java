4540e2022b0a282cb4c108661584941779eb4f72
jvmaval <jose.v.herraez.@gmail.com>
2016-07-15 08:49:34 +0200

    adasd

--- src/android/TAGS.java
+++ src/android/TAGS.java
@@ -39,7 +39,8 @@ public class TAGS extends CordovaPlugin {
     public void onNewIntent(Intent intent){
 		Log.w("myApp", "PROCESSING INTENT");
 		setIntent(intent);
-		super.onNewIntent(intent);       
+		super.onNewIntent(intent); 
+		processNfcIntent(intent);
     }
 	
 	
@@ -100,6 +101,40 @@ public class TAGS extends CordovaPlugin {
 	
 	private PendingIntent getPendingIntent() {
         return pendingIntent;
+    }
+	
+	public static void processNfcIntent(Intent intent){
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        if(tag != null){
+            Log.w("myApp", "TAG NOT NULL");
+            // set up read command buffer
+            byte blockNo = 0; // block address
+            byte[] id = tag.getId();
+            byte[] readCmd = new byte[3 + id.length];
+            readCmd[0] = 0x20; // set "address" flag (only send command to this tag)
+            readCmd[1] = 0x20; // ISO 15693 Single Block Read command byte
+
+            System.arraycopy(id, 0, readCmd, 2, id.length); // copy ID
+            readCmd[2 + id.length] = blockNo; // 1 byte payload: block address
+
+            NfcV tech = NfcV.get(tag);
+            if (tech != null) {
+                // send read command
+                try {
+                    tech.connect();
+                    byte[] data = tech.transceive(readCmd);
+                    Log.w("myApp", arraytoTag(data));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        tech.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 		
 	
